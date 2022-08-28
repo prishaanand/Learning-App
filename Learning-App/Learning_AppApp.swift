c3e558c667e3bc538eed0cd8c6c40941c1b30880
prishaanand <60487925+prishaanand@users.noreply.github.com>
2022-08-28 12:54:19 -0700

    Initial Commit

---
+++ Learning-App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning-App
+//
+//  Created by Prisha Anand on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
